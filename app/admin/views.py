32a0fc9048512d51a75c37ae16df2eb0767ac1d5
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-03-23 15:14:44 +0300

    [feature 156018243] write test for admin can add book

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,2 @@
-from . import admin
 
-class Admin(object):
-
-    @admin.route()
-    def add_title(self):
 
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,26 +1,61 @@
 import unittest
+import json
 
 # Local imports
-from app.admin.views import Admin
+from app import create_app
 
 """
 Admin can add, update and delete a book.
 The book information is book title, authors, publisher, year and ISNB
-normal: {book_title -> string, authors -> string, publisher -> string ISNB -> number}
+normal: {book_title -> string, authors -> list containing string, publisher -> string, year -> date ISNB -> number}
 boundaries: {book_title -> ["", "  ", 94, #%]: authors -> ["", "  ", 94, #%]: publisher -> ["", "  ", 94, #%]
             ISNB -> [["", "  ", #%, "akk"]}
 edge: {book_title -> "too long", }
-unexpected: {dict, list, file, }
+unexpected: {file, }
 
 """
 
 
 class AdminTestCase(unittest.TestCase):
-    def test_empty_title(self):
-        app = Admin()
-        result = app.add_title()
-        self.assertEqual(result, "Title should not be empty")
+
+    def setUp(self):
+        self.app = create_app(config_name="testing")
+        # Init the test client
+        self.client = self.app.test_client
+        # Json data for the predefined parameters
+        self.add_book_data = {
+            'book_title': 'The Wonder Boy',
+            'authors': ['john doe', 'doe Jacob'],  # make sure authors is a list
+            'publisher': 'Longhorn',
+            'Year': 2006,
+            'ISNB': 23-1223322-233
+        }
+
+    def test_add_book(self):
+        """
+        Test that admin should add book
+        :return: add_book_data
+        """
+        res = self.client().post('/admin/api/v1/add_book', data=self.add_book_data)
+        # Getting the results
+        result = json.loads(res.data.decode())
+        # make an assert of the success addition of books
+        self.assertEqual(result['message'], "Admin added book successfully.")
+        self.assertEqual(res.status_code, 201)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
