c248945186ce473c38bdde874f6712fc97640275
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-03-23 19:09:35 +0300

    [Test 156231762] rename auth test file

---
+++ tests/test_auth.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class AuthTestCase(unittest.TestCase):
+
+    def test_registration(self):
+        self.assertEqual(True, False)
+
+    def test_login(self):
+        self.assertEqual(True, False)
+
+    def test_reset_password(self):
+        self.assertEqual(True, False)
+
+    def test_logout(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
