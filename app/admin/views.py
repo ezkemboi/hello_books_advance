ee85532fd03bc1de52ee00826d636dd966f6cb8f
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-03-24 19:14:36 +0300

    [Fix] code according to codeacademy report

--- app/admin/views.py
+++ app/admin/views.py
@@ -20,22 +20,15 @@ class Admin(object):
         publisher = request.json.get('publisher')
         year = request.json.get('year')
         isnb = request.json.get('isnb')
-        if book_title is not None:
-            if authors is not None:
-                if publisher is not None:
-                    if year is not None:
-                        if isnb is not None:
-                            book_details['book_title'] = book_title
-                            book_details['authors'] = authors
-                            book_details['publisher'] = publisher
-                            book_details['year'] = year
-                            book_details['isnb'] = isnb
-                            books.append(book_details)
-                            return jsonify({'message': "Added the book successfully."})
-                        return jsonify({'message': "Fill in ISNB"})
-                    return jsonify({'message': "Fill in the year published"})
-                return jsonify({'message': "Fill in the publisher"})
-            return jsonify({'message': "Fill in the authors"})
-        return jsonify({'message': "Fill the book title"})
+        if book_title is not None and authors is not None and publisher is not None and year is not None and \
+                        isnb is not None:
+            book_details['book_title'] = book_title
+            book_details['authors'] = authors
+            book_details['publisher'] = publisher
+            book_details['year'] = year
+            book_details['isnb'] = isnb
+            books.append(book_details)
+            return jsonify({'message': "Added the book successfully."})
+        return jsonify({'message': "Fill all the details correctly."})
 
 
