e8974610edbd7708269629ffe6003e364046d32f
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-03-24 15:13:53 +0300

    [Feature 156019800] add register functionality

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,2 +1,12 @@
+from flask import request, jsonify, abort
 
+# local imports
+from . import admin
+
+
+class Admin(object):
+
+    @admin.route('/api/v1/books', methods=['POST'])
+    def add_book(self):
+        return "Added a book successfully"
 
---
+++ app/auth/views.py
@@ -0,0 +1,33 @@
+# global imports
+from flask import request, jsonify
+
+# local imports
+from . import auth
+
+users = []  # list that all users and their details
+user_details = {}  # dict that contain key and value of each entry
+
+
+class Auth(object):
+
+    @auth.route('/api/v1/auth/register', methods=['POST'])
+    def register(self):
+        email = request.json.get('email')
+
+        username = request.json.get('username').strip()
+
+        password = request.json.get('password').strip()
+
+        if email is not None and username is not None and password is not None:
+            if len(username) <= 4:
+                return jsonify({"Message": "Length of username should be more than 4"})
+            elif len(password) < 8:
+                return jsonify({"Message": "Minimum len of password is 8"})
+
+            else:
+                user_details['email'] = email
+                user_details['username'] = username
+                user_details['password'] = password
+                users.append(user_details)
+        return jsonify({"Message": "Email, Username and Password is required."})
+
