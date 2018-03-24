9c3b74ec3761910e0ca572aa66c24da3f68bbec0
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-03-24 11:46:15 +0300

    [Test] add login test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -28,6 +28,22 @@ class AuthTestCase(unittest.TestCase):
             'password': 'password12'
         }
 
+    # register and login helpers goes here
+    def register(self, email="test@example.com", username="test12", password="password12"):
+        user_data = {
+            'email': email,
+            'username': username,
+            'password': password
+        }
+        return self.client().post('/api/v1/auth/register', data=user_data)
+
+    def login(self, email="test@example.com", password="password12"):
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.client().post('/api/v1/auth/register', data=user_data)
+
     def test_registration(self):
         """
         Test user registration
@@ -40,8 +56,52 @@ class AuthTestCase(unittest.TestCase):
         self.assertEqual(result['message'], "You are registered successfully.")
         self.assertEqual(res.status_code, 201)
 
-    def test_login(self):
-        self.assertEqual(True, False)
+    def test_already_registered(self):
+        """
+        This test code helps to eliminate double registration
+        :return: error message
+        """
+        res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        second_res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        self.assertEqual(second_res.status_code, 202)
+        result = json.loads(second_res.data.decode())
+        self.assertEqual(result['message'], "Such a user already exist.")
+
+    def test_login_for_registered_user(self):
+        """
+        Test the login for user already registered.
+        :return: login successful
+        """
+        res = self.client().post('/api/v1/auth/register', data=self.user_data)
+        # return the code success for registration
+        self.assertEqual(res.status_code, 201)
+        # define login res
+        login_res = self.client().post('/api/v1/auth/login', data=self.user_data)
+
+        # decode the json data
+        result = json.loads(login_res.data.decode())
+        # returning the success message and code
+        self.assertEqual(result['message'], "Successfully logged in")
+        # return the result status code
+        self.assertEqual(login_res.status_code, 200)
+
+    def test_login_for_non_user(self):
+        """
+        Test non registered users cannot login
+        :return: error msg 401 (unauthorized)
+        """
+        not_user = {
+            'email': 'not_user@example.com',
+            'password': 'nope'
+        }
+        # make a post request from login
+        res = self.client().post('/api/v1/auth/login', data=not_user)
+        # receive the result in json
+        result = json.loads(res.data.decode())
+        # assert the response and return error status code 401
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(result['message'], "Invalid email or password, Please try again")
 
     def test_reset_password(self):
         self.assertEqual(True, False)
