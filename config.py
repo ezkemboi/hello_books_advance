ac9a079288be21f248b9478f65ef9e353631f09f
ezrqnkemboi <ezrqnkemboi@gmail.com>
2018-04-13 18:02:37 +0300

    [Chore #156763103] Move config file to main directory

---
+++ config.py
@@ -0,0 +1,56 @@
+"""
+This file contains all application configurations
+"""
+import os
+
+
+class Config(object):
+    """
+    All common configurations for app goes here.
+    """
+    DEBUG = False
+    CSRF_ENABLED = True
+    TESTING = False
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+    """
+    Only development configurations goes here.
+    """
+    DEBUG = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/hellobooks"
+
+
+class TestingConfig(Config):
+    """
+    Configuration for testing goes here.
+    """
+    TESTING = True
+    DEBUG = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/hello_books_test"
+
+
+class StagingConfig(Config):
+    """
+    Contains all configurations for staging stage
+    """
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """
+    Only production configurations goes here.
+    """
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig
+}
